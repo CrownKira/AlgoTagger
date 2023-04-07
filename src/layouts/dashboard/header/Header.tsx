b43ca1f1edba7230bf6597c107c1eece18ad574f
Kar Wi <t.karwi@yahoo.com>
2023-04-08 00:30:21 +0800

    chore: Add logo

--- src/layouts/dashboard/header/Header.tsx
+++ src/layouts/dashboard/header/Header.tsx
@@ -30,43 +30,15 @@ export default function Header({ onOpenNav }: Props) {
 
   const { themeLayout } = useSettingsContext();
 
-  const isNavHorizontal = themeLayout === 'horizontal';
+  // const isNavHorizontal = themeLayout === 'horizontal';
 
-  const isNavMini = themeLayout === 'mini';
+  // const isNavMini = themeLayout === 'mini';
 
-  const isDesktop = useResponsive('up', 'lg');
+  // const isDesktop = useResponsive('up', 'lg');
 
-  const isOffset = useOffSetTop(HEADER.H_DASHBOARD_DESKTOP) && !isNavHorizontal;
+  // const isOffset = useOffSetTop(HEADER.H_DASHBOARD_DESKTOP) && !isNavHorizontal;
 
-  const renderContent = (
-    <>
-      {isDesktop && isNavHorizontal && <Logo sx={{ mr: 2.5 }} />}
-
-      {!isDesktop && (
-        <IconButton onClick={onOpenNav} sx={{ mr: 1, color: 'text.primary' }}>
-          <Iconify icon="eva:menu-2-fill" />
-        </IconButton>
-      )}
-
-      <Searchbar />
-
-      <Stack
-        flexGrow={1}
-        direction="row"
-        alignItems="center"
-        justifyContent="flex-end"
-        spacing={{ xs: 0.5, sm: 1.5 }}
-      >
-        <LanguagePopover />
-
-        <NotificationsPopover />
-
-        <ContactsPopover />
-
-        <AccountPopover />
-      </Stack>
-    </>
-  );
+  const renderContent = <Logo sx={{ mr: 2.5 }} />;
 
   return (
     <AppBar
@@ -80,22 +52,24 @@ export default function Header({ onOpenNav }: Props) {
         transition: theme.transitions.create(['height'], {
           duration: theme.transitions.duration.shorter,
         }),
-        ...(isDesktop && {
-          width: `calc(100% - ${NAV.W_DASHBOARD + 1}px)`,
-          height: HEADER.H_DASHBOARD_DESKTOP,
-          ...(isOffset && {
-            height: HEADER.H_DASHBOARD_DESKTOP_OFFSET,
-          }),
-          ...(isNavHorizontal && {
-            width: 1,
-            bgcolor: 'background.default',
-            height: HEADER.H_DASHBOARD_DESKTOP_OFFSET,
-            borderBottom: `dashed 1px ${theme.palette.divider}`,
-          }),
-          ...(isNavMini && {
-            width: `calc(100% - ${NAV.W_DASHBOARD_MINI + 1}px)`,
-          }),
-        }),
+        // width: `calc(100% - ${NAV.W_DASHBOARD + 1}px)`,
+        height: HEADER.H_DASHBOARD_DESKTOP,
+        // ...(isDesktop && {
+        //   width: `calc(100% - ${NAV.W_DASHBOARD + 1}px)`,
+        //   height: HEADER.H_DASHBOARD_DESKTOP,
+        //   ...(isOffset && {
+        //     height: HEADER.H_DASHBOARD_DESKTOP_OFFSET,
+        //   }),
+        //   ...(isNavHorizontal && {
+        //     width: 1,
+        //     bgcolor: 'background.default',
+        //     height: HEADER.H_DASHBOARD_DESKTOP_OFFSET,
+        //     borderBottom: `dashed 1px ${theme.palette.divider}`,
+        //   }),
+        //   ...(isNavMini && {
+        //     width: `calc(100% - ${NAV.W_DASHBOARD_MINI + 1}px)`,
+        //   }),
+        // }),
       }}
     >
       <Toolbar
