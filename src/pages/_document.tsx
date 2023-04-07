9c33ccf5a88b4431fc94adf2d23529c4f5125c8e
Kar Wi <t.karwi@yahoo.com>
2023-04-08 02:07:09 +0800

    chore: Add favicon

---
+++ src/pages/_document.tsx
@@ -0,0 +1,89 @@
+import * as React from 'react';
+// next
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+// @emotion
+import createEmotionServer from '@emotion/server/create-instance';
+// utils
+import createEmotionCache from '../utils/createEmotionCache';
+// theme
+import palette from '../theme/palette';
+import { primaryFont } from '../theme/typography';
+
+// ----------------------------------------------------------------------
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en" className={primaryFont.className}>
+        <Head>
+          <meta charSet="utf-8" />
+          <link rel="manifest" href="/manifest.json" />
+
+          {/* PWA primary color */}
+          <meta name="theme-color" content={palette('light').primary.main} />
+
+          {/* Favicon */}
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+
+          {/* Emotion */}
+          <meta name="emotion-insertion-point" content="" />
+          {(this.props as any).emotionStyleTags}
+
+          {/* Meta */}
+          <meta
+            name="description"
+            content="The revolutionary translation app that takes into account the context and cultural nuances of your translations. Say goodbye to awkward or misinformed translations and hello to accurate, contextually aware communication."
+          />
+          <meta
+            name="keywords"
+            content="contextual translation, translation app, language learning, accurate translations, cultural context, effective communication, in-built dictionary, multiple translations, context explanations, language learner tool"
+          />
+          <meta name="author" content="NativeSay" />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+// ----------------------------------------------------------------------
+
+MyDocument.getInitialProps = async (ctx) => {
+  const originalRenderPage = ctx.renderPage;
+
+  const cache = createEmotionCache();
+
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App: any) =>
+        function EnhanceApp(props) {
+          return <App emotionCache={cache} {...props} />;
+        },
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      key={style.key}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
+
+  return {
+    ...initialProps,
+    emotionStyleTags,
+  };
+};
